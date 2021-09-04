3f59dc8e550dd837a3dc564cc8dd33b7b9cae080
Rutger Broekhoff <rutger@viasalix.nl>
2021-09-04 19:17:41 +0200

    Allow running wes with build.sh

--- wes/build.sh
+++ wes/build.sh
@@ -10,3 +10,8 @@ fi
 
 $CC main.c ${CFLAGS:-} -g3 -I.. -o wes -std=gnu17 -O2 -DENABLE_ASSERT -DENABLE_UNREACHABLE -lmbedcrypto -lmbedtls -lmbedx509 -Wall -Wextra -Wpedantic -Wformat=2 -Wformat-overflow=2 -Wformat-truncation=2 -Wformat-security -Wnull-dereference -Wstack-protector -Wtrampolines -Walloca -Wvla -Warray-bounds=2 -Wimplicit-fallthrough=3 -Wshift-overflow=2 -Wcast-qual -Wstringop-overflow=4 -Wconversion -Warith-conversion -Wlogical-op -Wduplicated-cond -Wduplicated-branches -Wformat-signedness -Wshadow -Wstrict-overflow=4 -Wundef -Wstrict-prototypes -Wswitch-default -Wstack-usage=1000000 -Wcast-align=strict -D_FORTIFY_SOURCE=2 -fstack-protector-strong -fstack-clash-protection -fPIE -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wl,-z,separate-code -fsanitize=address -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=leak -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=bounds-strict -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -Wno-unused-function -Werror
 
+if [[ -n "${1:-}" ]] && [[ "$1" == "run" ]]; then
+	shift
+	ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:detect_invalid_pointer_pairs=2 ./wes $@
+fi
+
